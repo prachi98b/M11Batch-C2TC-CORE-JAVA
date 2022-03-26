38b9068f2220e42cfd7413e6ce4046faceb79dec
prach <prach@LAPTOP-JTQFL6K2>
2022-03-26 11:49:56 +0530

    new repository commit

---
+++ MyJavaPrograms/src/MyJavaPrograms/EvenOddCode.java
@@ -0,0 +1,21 @@
+package MyJavaPrograms;
+
+import java.util.Scanner;
+
+public class EvenOddCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc= new Scanner(System.in);
+		int num = 5;
+		System.out.println("Enter a number:");
+		num = sc.nextInt();
+		if (num % 2 == 0)
+			System.out.println("The entered number is even.");
+		else 
+			System.out.println("The entered number is odd.");
+
+	}
+
+}
---
+++ MyJavaPrograms/src/MyJavaPrograms/HelloWorldCode.java
@@ -0,0 +1,10 @@
+package MyJavaPrograms;
+
+public class HelloWorldCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
