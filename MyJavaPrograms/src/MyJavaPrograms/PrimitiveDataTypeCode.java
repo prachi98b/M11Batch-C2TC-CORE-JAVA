9bb27e88cc0c12a74a9a83acbac1125b337daaa6
prach <prach@LAPTOP-JTQFL6K2>
2022-03-26 12:31:05 +0530

    new repository commit

---
+++ MyJavaPrograms/src/MyJavaPrograms/PrimitiveDataTypeCode.java
@@ -0,0 +1,28 @@
+package MyJavaPrograms;
+
+public class PrimitiveDataTypeCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int max = 2147483647;
+		int min = -2147483648;
+		short shortMax = 32767;
+		short shortMin = -32768;
+		long longMax = 9223372036854775807L;
+		long longMin = -9223372036854775808L;
+		byte byteMax = 127;
+		byte byteMin = -128;
+		
+		System.out.println("int max: " + max);
+		System.out.println("int min: " +min);
+		System.out.println("short Max: "+ shortMax);
+		System.out.println("long max: "+ longMax);
+		System.out.println("short min: "+ shortMin);
+		System.out.println("long min: "+ longMin);
+		System.out.println("byte max: "+ byteMax);
+		System.out.println("byte min: "+ byteMin);
+
+	}
+
+}
